ce321e767f73fc49f7d7db2f6c46f786f9e9020c
PaoloBaltazar <gpaolobaltazar@gmail.com>
2024-08-07 14:27:08 +0800

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,10 @@
+const names = require('./4-names')
+const sayHi = require('./4-utils')
+const data = require('./4-alternative-variables')
+
+console.log(names);
+
+sayHi(names.john)
+sayHi(names.pao)
+
+console.log(data)
---
+++ 4-names.js
@@ -0,0 +1,4 @@
+const john = 'john';
+const pao = 'pao';
+
+module.exports = { john, pao }
